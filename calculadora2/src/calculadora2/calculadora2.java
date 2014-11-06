d98983446c1967020d12043a1ee660ea8cc66f1c
Sergi Maduell <maduelo@hotmail.com>
2014-11-06 20:10:56 +0100

    Calculadora cientifica

--- calculadora2/src/calculadora2/calculadora2.java
+++ calculadora2/src/calculadora2/calculadora2.java
@@ -3,7 +3,7 @@ package calculadora2;
 public class calculadora2 {
 
 	public static void main(String[] args) {
-		System.out.println("Sóc una calculadora simple");
+		System.out.println("Sóc una calculadora cientifica");
 
 	}
 
